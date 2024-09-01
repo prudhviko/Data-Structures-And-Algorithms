a9ee27f2084c8cfec63caf754a0b665c60acf972
prudhviko <prudhvikovagana@gmail.com>
2024-09-01 22:43:41 +0530

    Convert Temparature From Celsius To Farenheit

---
+++ 02-ConvertCelsiusToFahrenheit.js
@@ -0,0 +1,9 @@
+// Given a temperature in celsius c. You need to convert the given temperature to Fahrenheit.
+
+function celsiusToFahrenheit(c) {
+  return (c * 9) / 5 + 32;
+}
+
+console.log(celsiusToFahrenheit(0));
+console.log(celsiusToFahrenheit(25));
+console.log(celsiusToFahrenheit(-10));
