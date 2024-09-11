36874c79c7e6984bc8991a471488cf507fb60e36
prudhviko <prudhvikovagana@gmail.com>
2024-09-11 22:35:48 +0530

    largest and second largest

---
+++ Arrays/04-largestAndSecondLargest.js
@@ -0,0 +1,15 @@
+//  largest and second largest element
+
+function largestAndSecondLargest(arr) {
+  let uniqueArray = Array.from(new Set(arr));
+
+  uniqueArray.sort((a, b) => a > b);
+
+  if (uniqueArray.length < 2) {
+    return [uniqueArray[0], -1];
+  } else {
+    return [uniqueArray[uniqueArray[0]], uniqueArray[uniqueArray[1]]];
+  }
+}
+
+console.log(largestAndSecondLargest([1, 2, 3, 4, 5])); // [5, 4]
