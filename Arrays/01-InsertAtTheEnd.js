995af1cd98d19555d206449d24eb0fef55e24c74
prudhviko <prudhvikovagana@gmail.com>
2024-09-11 22:04:37 +0530

    Fibonacci & Insert Element At The End

---
+++ Arrays/01-InsertAtTheEnd.js
@@ -0,0 +1,6 @@
+function insertEnd(arr, element) {
+  arr.push(element);
+  return arr;
+}
+
+console.log(insertEnd([1, 2, 3, 4, 5], 6)); // [1,2,3,4,5,6]
--- Recursion/05-fibonacci.js
+++ Recursion/05-fibonacci.js
@@ -1,13 +1,8 @@
-function toh(n, from_rod, to_rod, aux_rod) {
-  if (n === 0) {
-    return 0;
-  }
+// You are given a number n. You need to find nth Fibonacci number.
 
-  const moves = toh(n - 1, from_rod, aux_rod, to_rod);
-  console.log(`move disk ${n} from rod ${from_rod} to rod ${to_rod}`);
-  moves++;
-  moves += toh(n - 1, aux_rod, to_rod, from_rod);
-  return moves;
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-console.log(toh(3, "A", "C", "B"));
+console.log(fibonacci(9));
