8c9b1503c63d75847a6fc43689f99b9d497d8501
prudhviko <prudhvikovagana@gmail.com>
2024-09-12 11:08:56 +0530

    file names

--- Arrays/05-MaximumAdjacent.js
+++ Arrays/05-MaximumAdjacent.js
@@ -1,6 +1,6 @@
 //  find maximum for every adjacent pairs in the array.
 
-function adjacentArray(arr) {
+function maximumAdjacent(arr) {
   let max = [];
   for (let i = 0; i < arr.length - 1; i++) {
     max.push(Math.max(arr[i], arr[i + 1]));
@@ -8,4 +8,4 @@ function adjacentArray(arr) {
   return max;
 }
 
-console.log(adjacentArray([1, 2, 3, 4, 5])); // [2, 3, 4, 5]
+console.log(maximumAdjacent([1, 2, 3, 4, 5])); // [2, 3, 4, 5]
