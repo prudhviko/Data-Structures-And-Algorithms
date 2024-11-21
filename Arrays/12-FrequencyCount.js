a80598928f6472223d9206877f19d219a5f3469d
prudhviko <prudhvikovagana@gmail.com>
2024-11-21 21:23:55 +0530

    Frequency Array Problem Done

--- Arrays/12-FrequencyCount.js
+++ Arrays/12-FrequencyCount.js
@@ -1,3 +1,13 @@
 // You are given an array arr[] containing positive integers. These integers can be from 1 to p, and some numbers may be repeated or absent. Your task is to count the frequency of all numbers that lie in the range 1 to n.
 
-function frequencyCount() {}
+function frequencyCount(arr) {
+  let freqArr = Array(arr.length).fill(0);
+
+  for (let i = 0; i < arr.length; i++) {
+    freqArr[arr[i] - 1] += 1;
+  }
+
+  return freqArr;
+}
+
+console.log(frequencyCount([2, 3, 2, 3, 5]));
